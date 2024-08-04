c6209263a15f684e6bb50674db7b3b6a5b6bec32
muthukrishnan-112 <muthukrishnan6702@gmail.com>
2024-08-04 13:35:19 +0530

    .

---
+++ frontend/src/componends/ProductCard.js
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom'
+
+export default function ProductCard({product}){
+  return <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+          <div className="card p-1  rounded">
+            <img
+              className="card-img-top mx-0" 
+              src={product.images[0].image} alt='img1'/>
+
+            <div className="card-body d-flex flex-column">
+              <h5 className="card-title">
+                <Link to={"/product/"+product._id}>{product.name}</Link>
+              </h5>
+              <div className="ratings mt-auto">
+                <div className="rating-outer">
+                  <div className="rating-inner" style={{width:`${product.ratings/5 *100}%`}}></div> 
+                </div>
+              </div>
+              <p className="card-text">${product.price}</p>
+              <Link to={"/product/"+product._id} id="view_btn" className="btn btn-block mx-2">View Details</Link>
+            </div>
+          </div>
+        </div>
+
+}
